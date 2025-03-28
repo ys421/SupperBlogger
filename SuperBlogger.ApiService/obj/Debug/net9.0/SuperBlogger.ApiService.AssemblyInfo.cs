5e8128814246d63c5dc0220f5172b4bc0feef6e9
ys421 <boy09021313@gmail.com>
2025-03-28 01:00:20 -0700

    Save current changes

--- SuperBlogger.ApiService/obj/Debug/net9.0/SuperBlogger.ApiService.AssemblyInfo.cs
+++ SuperBlogger.ApiService/obj/Debug/net9.0/SuperBlogger.ApiService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuperBlogger.ApiService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a39b8aa1781ccc3427abc5410f12e61cea9ceac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5e9dbbe92b6b6899313187bd3e5ea8cc37b1a09")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuperBlogger.ApiService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuperBlogger.ApiService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
